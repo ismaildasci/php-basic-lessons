faefab2ea260a0cc5a5dbc7eef17c799e638d994
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-04-21 22:06:26 +0300

    database class

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 require 'functions.php';
+require 'Database.php';
 
-// require 'router.php';
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-// connect to your MySQL database.
-$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
-$pdo = new PDO($dsn);
-$statement = $pdo->prepare('SELECT * FROM posts');
-$statement->execute();
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li> $post[title] </li>";
-}
+dd($posts);
