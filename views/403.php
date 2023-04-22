8eb38d2841a48cac711d9eebf49fde6433ebb3ab
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-04-22 12:47:06 +0300

    Authorization

---
+++ views/403.php
@@ -0,0 +1,13 @@
+<?php require 'views/partials/head.php'; ?>
+<?php require 'views/partials/nav.php'; ?>
+
+<main>
+    <div class="mx-auto mx-w-7xl py-6 sm:px-6 lg:px8">
+        <h1 class="text-2xl font-bold">You are not authorized to view this page.</h1>
+
+        <p class="mt-4">
+            <a href="/" class="text-blue-500 underline">go back...</a>
+        </p>
+    </div>
+</main>
+<?php require 'views/partials/footer.php'; ?>
